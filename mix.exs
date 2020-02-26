80a6733049e0597a01d20d0d3e5f96ea5a9f9d97
dkuku <daniel.kuku@gmail.com>
2020-02-26 19:33:55 +0000

    comment out artifacts

--- mix.exs
+++ mix.exs
@@ -34,9 +34,9 @@ defmodule NervesSystemOpiPC.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_sites: [
-        {:github_releases, "dkuku/#{@app}"}
-      ],
+      #artifact_sites: [
+      #  {:github_releases, "dkuku/#{@app}"}
+      #],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
